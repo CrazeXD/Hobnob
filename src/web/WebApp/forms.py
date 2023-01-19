3d0f86f96c5d46d672b82057b157ce1645d899cf
Rishabh Garg <90533906+CrazeXD@users.noreply.github.com>
2023-01-19 04:40:49 +0000

    Revert "Added school selector model"
    This reverts commit 1bc732569bbf4a9b6785ca1993dd9a7d34473e3d.

--- src/web/WebApp/forms.py
+++ src/web/WebApp/forms.py
@@ -8,15 +8,7 @@ class SignupForm(forms.ModelForm):
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'grade', 'pronouns', 'password', 'school']
 
-# TODO: Create form template using this form class and save the user's school
-# Integrate with signp view
-class SchoolSelector(forms.Form):
-    def __init__(self, *args, **kwargs):
-        super(SchoolSelector, self).__init__(*args, **kwargs)
-        choices = [(i, i) for i in kwargs['schools']]
-        self.fields['school'].choices = choices
-    choices = []
-    school = forms.ChoiceField(choices=choices)
+
 class LoginForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput())
     password = forms.CharField(widget=forms.PasswordInput())
@@ -26,4 +18,4 @@ class UserEditForm(forms.ModelForm):
     class Meta:
         model = User
         # TODO: Add school validator
-        fields = ['username', 'email', 'first_name', 'last_name', 'grade', 'pronouns']
+        fields = ['username', 'email', 'first_name', 'last_name', 'grade', 'pronouns', 'school']
