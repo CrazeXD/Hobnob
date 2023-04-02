d65a64df4179f80fe09bf60b0bbc281a3cf849ef
Craze <CrazeXD@github.com>
2023-04-02 16:16:26 -0700

    Switched to environ

--- src/web/Hobnob/settings.py
+++ src/web/Hobnob/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-file = open("secret_key.txt", "r")
-SECRET_KEY = file.read().strip()
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
