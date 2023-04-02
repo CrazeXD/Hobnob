07ce30354e7057a34b19a3b92056dbd6fe83e96d
Craze <CrazeXD@github.com>
2023-04-02 15:44:33 -0700

    Changed secret key location

--- src/web/Hobnob/settings.py
+++ src/web/Hobnob/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-file = open("D:\CodingProjects\Hobnob\secret_key.txt", "r")
+file = open("secret_key.txt", "r")
 SECRET_KEY = file.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
