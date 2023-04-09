be68b01c350930b50566e43f3d6ce1b798c4d748
Craze <CrazeXD@github.com>
2023-04-09 09:55:54 -0700

    Removed captcha after errors

--- src/web/Hobnob/urls.py
+++ src/web/Hobnob/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('WebApp.urls')),
-    path('captcha/', include('captcha.urls')),
 ]
